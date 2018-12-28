7673e1b2c743b93db30f52b1e5f978e494d18029
92pino <jjb8382@gmail.com>
2018-12-28 21:57:48 +0900

    Welcome Msg, GoToLoginPage add

--- PersonalProject/PersonalProject/ViewController.swift
+++ PersonalProject/PersonalProject/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var WelcomeMsg: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    @IBAction func WelcomeBtn(_ sender: Any) {
+        self.WelcomeMsg.text = "로그인 하시려면 로그인 버튼을 눌러주세요"
+    }
+    
 
-
+    @IBAction func GoToLogin(_ sender: Any) {
+        guard let uvc = self.storyboard?.instantiateViewController(withIdentifier: "LoginPage") else {
+            return
+        }
+        
+        uvc.modalTransitionStyle = UIModalTransitionStyle.flipHorizontal
+        
+        self.present(uvc, animated: true)
+    }
 }
 
