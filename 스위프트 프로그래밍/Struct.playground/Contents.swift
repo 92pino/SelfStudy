bf6644a2a5cd599c6f4272b105852e8a08890e1d
92pino <jjb8382@gmail.com>
2019-04-21 17:25:37 +0900

    Class, Struct study

--- 스위프트 프로그래밍/Struct.playground/Contents.swift
+++ 스위프트 프로그래밍/Struct.playground/Contents.swift
@@ -1,5 +1,11 @@
 import UIKit
 
+/*:
+ ## 구조체
+ 구조체의 인스턴스는 값 타입
+ 상속 X
+ */
+
 struct BasicInformation {
     var name: String
     var age: Int
@@ -14,3 +20,52 @@ pinoInfo.name = "seba"
 let sebaInfo: BasicInformation = BasicInformation(name: "Seba", age: 99)
 // sebaInfo.age = 100
 // jennyInfo.age = 10
+
+/*:
+ ## 클래스
+ 클래스의 인스턴스는 참조 타입
+ 상속 O
+*/
+class Person {
+    var height: Float = 0.0
+    var weight: Float = 0.0
+}
+
+var pino:Person = Person()
+pino.height = 123.4
+pino.weight = 123.4
+
+let pinkeb:Person = Person()
+pinkeb.height = 123.4
+pinkeb.weight = 123.4
+
+// 클래스 인스턴스의 소멸 deinit
+// 더이상 참조할 필요가 없을 때 메모리에서 해제
+// 이 과정을 소멸이라고 하는데 소멸 되기 직전 deinit라는 메서드 호출
+
+class PersonDeinit {
+    var height: Float = 0.0
+    var weight: Float = 0.0
+    
+    deinit {
+        print("PersonDeinit 클래스의 인스턴스가 소멸")
+    }
+}
+
+var pino2: PersonDeinit? = PersonDeinit()
+pino2 = nil
+/*:
+ ### 구조체와 클래스의 공통점
+ - 값을 저장하기 위해 프로퍼티를 정의
+ - 기능 실행을 위해 메서드를 정의
+ - 서브스크립트 문법을 통해 구조체 또는 클래스가 갖는 값에 접근하도록 서브스크립트를 정의
+ - 초기화될 떄의 상태를 지정하기 위해 이니셜라이저를 정의
+ - 초기구현과 더불어 새로운 기능 추가를 위해 익스텐션을 통해 확장 가능
+ - 트정 기능을 실행하기 위해 특정 프로토콜을 준수
+ 
+ ### 구조체와 클래스의 차이점
+ - 구조체는 상속 불가
+ - 타입캐스팅은 클래스의 인스턴스에만 허용
+ - 디이니셜라이저는 클래스의 인스턴스에만 활용 가능
+ - 참조 횟수 계산은 클래스의 인스턴스에만 적용
+*/
