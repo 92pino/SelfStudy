396c64556a8533dfce7c583a8e3875d4ed3c8b86
92pino <jjb8382@gmail.com>
2019-04-23 17:08:28 +0900

    FlipAnimation simple

--- FlipAnimation/FlipAnimation/ViewController.swift
+++ FlipAnimation/FlipAnimation/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnCard: UIButton!
+    var isOpen = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnFlip(_ sender: Any) {
+        if isOpen {
+            isOpen = false
+            let image = UIImage(named: "BT2-1")
+            btnCard.setImage(image, for: .normal)
+            UIView.transition(with: btnCard, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+        } else {
+            isOpen = true
+            let image = UIImage(named: "BT5-3")
+            btnCard.setImage(image, for: .normal)
+            UIView.transition(with: btnCard, duration: 0.3, options: .transitionFlipFromRight, animations: nil, completion: nil)
+        }
+    }
+    
 }
 
