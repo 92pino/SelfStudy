a14fa6d5de5cf4d68742797c6c60b98aa15b065d
92pino <jjb8382@gmail.com>
2019-08-21 00:59:31 +0900

    property & method repeat

--- 스위프트 프로그래밍/PropertyMethod.playground/Contents.swift
+++ 스위프트 프로그래밍/PropertyMethod.playground/Contents.swift
@@ -102,161 +102,205 @@ import UIKit
 //pinoPosition3.oppositePoint = CoordinatePoint3(x: 15, y: 10)
 //print("3 : ", pinoPosition3)
 
-class Point {
-    // 연산한 값을 저장할 변수 tempX에 임의의 값 지정
-    // tempX의 값은 의미없는값???
-    var tempX: Int = 5
-    var x: Int {
-        get {
-            print("getValue :", x)
-            return x
-        }
+//class Point {
+//    // 연산한 값을 저장할 변수 tempX에 임의의 값 지정
+//    // tempX의 값은 의미없는값???
+//    var tempX: Int = 5
+//    var x: Int {
+//        get {
+//            print("getValue :", x)
+//            return x
+//        }
+//
+//        set {
+//            print("newValue : ", newValue)
+//            tempX = newValue * 2
+//        }
+//    }
+//}
+//var p: Point = Point()
+//p.x = 12
+//print(p.tempX)
+//
+//class ComputedProperty {
+//    var width = 5.0
+//    var height = 5.0
+//
+//    // lazyArea는 직접적으로 값을 바꾸지 않는 이상 초기 결과값을 계속 반환한다.
+//    // computed는 작성 할 때마다 값을 다시 계산해서 결과값을 반환한다.
+//    lazy var lazyArea = width * height
+//    var area: Double {
+//        return width * height
+//    }
+//
+//    // computed + stored
+//    // 연산한 값을 저장할 변수
+//    private var _koreanWon = 0.0
+//    var wonToDollar: Double {
+//        get {
+//            return _koreanWon / 1136.5
+//        }
+//
+//        set {
+//            print("newValue :", newValue)
+//            _koreanWon = newValue
+//        }
+//    }
+//}
+//
+//var computed = ComputedProperty()
+//computed.area
+//computed.lazyArea
+//
+//computed.width = 10
+//computed.area
+//computed.lazyArea
+//
+//// computed.lazyArea 값을 바꾸면 바꾼 값으로 계속 호출
+//// 값을 변경하지 않을 경우 기존 값 5.0 * 5.0 ==> 25로 계속 호출
+////computed.lazyArea = 50.0
+//computed.lazyArea
+//
+//computed.width = 20
+//computed.height = 20
+//computed.area
+//computed.lazyArea
+//
+//computed.width = 20
+//computed.height = 20
+//computed.area
+//computed.lazyArea
+//
+//computed.wonToDollar
+//computed.wonToDollar = 10000
+//computed.wonToDollar
+//
+//class Company {
+//    var _member: Int = 5
+//    var members: Int {
+//        get {
+//            return _member
+//        }
+//
+//        set {
+//            if (newValue < 1) {
+//                print("직원수는 1명보다 작을 수 없습니다.")
+//            } else {
+//                _member = newValue
+//            }
+//        }
+//    }
+//}
+//
+//print(Company().members)
+//
+//class Account {
+//    var credit: Int = 0 {
+//        willSet {
+//            print("잔액이 \(credit)원에서 \(newValue)원으로 변경될 예정입니다.")
+//        }
+//
+//        didSet {
+//            print("잔액이 \(oldValue)원에서 \(credit)원으로 변경되었습니다.")
+//        }
+//    }
+//
+//    var dollarValue: Double {
+//        get {
+//            return Double(credit)
+//        }
+//
+//        set {
+//            credit = Int(newValue * 1000)
+//            print("잔액을 \(newValue)달러로 변경 중입니다.")
+//        }
+//    }
+//}
+//
+//class ForeignAccount: Account {
+//    override var dollarValue: Double {
+//        willSet {
+//            print("잔액이 \(dollarValue)달러에서 \(newValue)달러로 변경될 예정입니다")
+//        }
+//
+//        didSet {
+//            print("잔액이 \(oldValue)달러에서 \(dollarValue)달러로 변경될 예정입니다")
+//        }
+//    }
+//}
+//
+//let myAccount: ForeignAccount = ForeignAccount()
+//
+//myAccount.credit = 1000
+//myAccount.dollarValue = 2
+//
+//class OnCreate {
+//    init() {
+//        print("OnCreate!!")
+//    }
+//}
+//
+//class LazyTest {
+//    var base = 0
+//    lazy var late = OnCreate()
+//
+//    init() {
+//        print("lazy Test")
+//    }
+//}
+//
+//let lz = LazyTest()
+//lz
+//
+//// 타입 프로퍼티
+//// 특정 타입에 사용되는 프로퍼티
+//
+//
+//// 프로퍼티 옵저버
+//// 프로퍼티의 값이 변하는 것을 감지
+//// 프로퍼티의 값이 변할 때 값의 변화에 따른 특정 액션을 실행
+//// 저장 프로퍼티에 적용, 부모클래스로부터 상속
 
-        set {
-            print("newValue : ", newValue)
-            tempX = newValue * 2
-        }
-    }
+// 저장 프로퍼티
+struct CoordinatePoint {
+    var x: Int
+    var y: Int
 }
-var p: Point = Point()
-p.x = 12
-print(p.tempX)
 
-class ComputedProperty {
-    var width = 5.0
-    var height = 5.0
+let yagomPoint: CoordinatePoint = CoordinatePoint(x: 10, y: 5)
+
+class Position {
+    var point: CoordinatePoint  // 저장 프로퍼티(변수) - point는 언제든 변경될 수 있음을 뜻함
+    let name: String            // 저장 프로퍼티(상수)
     
-    // lazyArea는 직접적으로 값을 바꾸지 않는 이상 초기 결과값을 계속 반환한다.
-    // computed는 작성 할 때마다 값을 다시 계산해서 결과값을 반환한다.
-    lazy var lazyArea = width * height
-    var area: Double {
-        return width * height
-    }
-    
-    // computed + stored
-    // 연산한 값을 저장할 변수
-    private var _koreanWon = 0.0
-    var wonToDollar: Double {
-        get {
-            return _koreanWon / 1136.5
-        }
-        
-        set {
-            print("newValue :", newValue)
-            _koreanWon = newValue
-        }
+    init(name: String, currentPoint: CoordinatePoint) {
+        self.name = name
+        self.point = currentPoint
     }
 }
 
-var computed = ComputedProperty()
-computed.area
-computed.lazyArea
+let yagomPosition: Position = Position(name: "yagom", currentPoint: yagomPoint)
 
-computed.width = 10
-computed.area
-computed.lazyArea
-
-// computed.lazyArea 값을 바꾸면 바꾼 값으로 계속 호출
-// 값을 변경하지 않을 경우 기존 값 5.0 * 5.0 ==> 25로 계속 호출
-//computed.lazyArea = 50.0
-computed.lazyArea
-
-computed.width = 20
-computed.height = 20
-computed.area
-computed.lazyArea
-
-computed.width = 20
-computed.height = 20
-computed.area
-computed.lazyArea
-
-computed.wonToDollar
-computed.wonToDollar = 10000
-computed.wonToDollar
-
-class Company {
-    var _member: Int = 5
-    var members: Int {
-        get {
-            return _member
-        }
-        
-        set {
-            if (newValue < 1) {
-                print("직원수는 1명보다 작을 수 없습니다.")
-            } else {
-                _member = newValue
-            }
-        }
-    }
+struct CoordinatePoint2 {
+    var x: Int = 0
+    var y: Int = 0
 }
 
-print(Company().members)
+var yagomPoint2: CoordinatePoint2 = CoordinatePoint2()
+yagomPoint2.x = 100
 
-class Account {
-    var credit: Int = 0 {
-        willSet {
-            print("잔액이 \(credit)원에서 \(newValue)원으로 변경될 예정입니다.")
-        }
-        
-        didSet {
-            print("잔액이 \(oldValue)원에서 \(credit)원으로 변경되었습니다.")
-        }
-    }
-    
-    var dollarValue: Double {
-        get {
-            return Double(credit)
-        }
-        
-        set {
-            credit = Int(newValue * 1000)
-            print("잔액을 \(newValue)달러로 변경 중입니다.")
-        }
-    }
+print(yagomPoint2.x)
+
+let wizplanPoint: CoordinatePoint2 = CoordinatePoint2(x: 10, y: 5)
+
+print(yagomPoint2.x, yagomPoint2.y)
+print(wizplanPoint.x, wizplanPoint.y)
+
+class Position2 {
+    var point: CoordinatePoint2 = CoordinatePoint2()
+    var name: String = "Unknown"
 }
 
-class ForeignAccount: Account {
-    override var dollarValue: Double {
-        willSet {
-            print("잔액이 \(dollarValue)달러에서 \(newValue)달러로 변경될 예정입니다")
-        }
-        
-        didSet {
-            print("잔액이 \(oldValue)달러에서 \(dollarValue)달러로 변경될 예정입니다")
-        }
-    }
-}
-
-let myAccount: ForeignAccount = ForeignAccount()
-
-myAccount.credit = 1000
-myAccount.dollarValue = 2
-
-class OnCreate {
-    init() {
-        print("OnCreate!!")
-    }
-}
-
-class LazyTest {
-    var base = 0
-    lazy var late = OnCreate()
-    
-    init() {
-        print("lazy Test")
-    }
-}
-
-let lz = LazyTest()
-lz
-
-// 타입 프로퍼티
-// 특정 타입에 사용되는 프로퍼티
-
-
-// 프로퍼티 옵저버
-// 프로퍼티의 값이 변하는 것을 감지
-// 프로퍼티의 값이 변할 때 값의 변화에 따른 특정 액션을 실행
-// 저장 프로퍼티에 적용, 부모클래스로부터 상속
+let yagomPosition2: Position2 = Position2()
+yagomPosition2.point = yagomPoint2
+yagomPosition2.name = "yagom"
