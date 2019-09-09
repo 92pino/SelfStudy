110cfc60cf0b7c4a343826da924df6bd36beaeb5
92pino <jjb8382@gmail.com>
2019-09-09 15:39:50 +0900

    Optional Chaining study

--- 스위프트 프로그래밍/OptionalChaining.playground/Contents.swift
+++ 스위프트 프로그래밍/OptionalChaining.playground/Contents.swift
@@ -1,3 +1,66 @@
 import UIKit
 
-var str = "Hello, playground"
+class Room {
+    var number: Int
+    
+    init(number: Int) {
+        self.number = number
+    }
+}
+
+class Building {
+    var name: String
+    var room: Room?
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+struct Address {
+    var province: String
+    var city: String
+    var street: String
+    var building: Building?
+    var detailAddress: String?
+}
+
+class Person {
+    var name: String
+    var address: Address?
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+let yagom: Person = Person(name: "pino")
+
+let pinoRoomViaOptionalChaining: Int? = yagom.address?.building?.room?.number
+
+// 옵셔널 바인딩
+
+var roomNumber: Int? = nil
+
+if let yagomAddress: Address = yagom.address {
+    if let yagomBuilding: Building = yagomAddress.building {
+        if let yagomRoom: Room = yagomBuilding.room {
+            roomNumber = yagomRoom.number
+        }
+    }
+}
+
+if let number: Int = roomNumber {
+    print(number)
+} else {
+    print("Can not find room number")
+}
+
+// 옵셔널 체이닝
+if let roomNumber2: Int = yagom.address?.building?.room?.number {
+    print(roomNumber)
+} else {
+    print("Can not find room number")
+}
+
+
