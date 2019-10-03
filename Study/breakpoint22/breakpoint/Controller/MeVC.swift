79cd2b6cf1ddd5840d338450e3ed5cc587a5c00d
92pino <jjb8382@gmail.com>
2019-10-03 22:46:25 +0900

    firebase 연동 메시지 받아오기 성공

---
+++ Study/breakpoint22/breakpoint/Controller/MeVC.swift
@@ -0,0 +1,24 @@
+//
+//  MeVC.swift
+//  breakpoint
+//
+//  Created by JinBae Jeong on 2019/10/03.
+//  Copyright © 2019 brian. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func signOutBtnWasPressed(_ sender: Any) {
+    }
+    
+}
---
+++ Study/breakpoint22/breakpoint/View/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  breakpoint
+//
+//  Created by JinBae Jeong on 2019/10/03.
+//  Copyright © 2019 brian. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var contentLbl: UILabel!
+    
+    func configureCell(profileImage: UIImage, email: String, content: String) {
+        self.profileImage.image = profileImage
+        self.emailLbl.text = email
+        self.contentLbl.text = content
+    }
+    
+}
